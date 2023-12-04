41060b8c131d259e48627f3b405c86add1640422
SifisoSithole <sitholesifisobrian@gmail.com>
2023-12-04 11:36:45 +0200

    Implement Two-Player Mode with Score Tracking
    Extend the Tic-Tac-Toe game to support a continuous two-player mode where players can compete against each other indefinitely. The game now keeps track of individual player scores, and draws are also considered in the scoring system. Player turns alternate, and the game continues until players decide to stop. The code enhancements include score tracking, improved turn management, and updated UI to reflect the ongoing game state. This commit adds a more complete and interactive gaming experience for two players.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 
 function createPlayer(name, playerMarker){
-    const score = 0;
-    return {name, playerMarker, score};
+    let score = 0;
+
+    function increaseScore(){
+        score += 1;
+    };
+
+    return {name, playerMarker, score, increaseScore };
 };
 
 const Board = (() => {
@@ -59,6 +64,7 @@ const GameController = (() => {
     const doublePlayerMode = document.getElementById('double-player');
     const modal = document.querySelector('[data-modal]');
     const gamePlay = document.querySelector('#start-game');
+    
 
     doublePlayerMode.addEventListener('click', () => {
         const gameSetUp = document.querySelector('.game-setup');
@@ -70,30 +76,99 @@ const GameController = (() => {
 
     gamePlay.addEventListener('click', () => {
         const gameCells = document.querySelectorAll('.cells');
-        let currentPlayer = 'player1';
+        const statusModal = document.querySelector('#show-winner');
+        const winnerHeading = document.querySelector('#show-winner > h1');
+        const playAgain = document.querySelector('#repeat');
+        const endGame = document.querySelector('#end-game');
         const game = Board;
         const controller = GameController;
+        let currentPlayer = 'player1';
+        let noTies = 0;
+       
+
+        function changeScore(){
+            const player1Score = document.querySelector('#player-1-score');
+            const player2Score = document.querySelector('#player-2-score');
+            const drawScore = document.querySelector('#draw-score');
+
+            drawScore.innerHTML = `Ties: ${noTies}`;
+            player1Score.innerHTML = `${players.player1.name}: ${players.player1.score}`;
+            player2Score.innerHTML = `${players.player2.name}: ${players.player2.score}`;
+        }
+
+        playAgain.addEventListener('click', () => {
+
+            Object.values(game.DOMBoardCache).forEach(cell => {
+                cell.innerHTML = '';
+            });
+
+            changeScore();
+            statusModal.style.display = 'none';
+            statusModal.close();
+        })
+
+        endGame.addEventListener('click', () => {
+            location.reload();
+        });
+
         const players = (function addPlayers(){
-            const player1Name = document.querySelector('#player-1-name');
-            const player2Name = document.querySelector('#player-2-name');
+            let player1Name = document.querySelector('#player-1-name').value;
+            let player2Name = document.querySelector('#player-2-name').value;
+            const player1Score = document.querySelector('#player-1-score');
+            const player2Score = document.querySelector('#player-2-score');
+
+            if (player1Name === '')
+                player1Name = 'Player-X';
+
+            if (player2Name === '')
+                player2Name = 'Player-O';
 
             const player1 = createPlayer(player1Name, 'X');
             const player2 = createPlayer(player2Name, 'O');
 
             return { player1, player2 };
+
         })();
+
+        changeScore();
+
+        (function displayBoard() {
+            const gameContainer = document.querySelector('.game-container');
+
+            modal.style.display = 'none';
+            modal.close();
+            gameContainer.style.display = 'block';
+        })();
+
+
 
         let isPlayer1 = true;
         gameCells.forEach(cell => {
-            cell.addEventListener('click', (e) => {
+            cell.addEventListener('click', () => {
                 const cellIndex = cell.getAttribute('cell-index');
-                let roundResult;
+                let gameBoardFull = true;
 
                 game.addMarker(players[currentPlayer].playerMarker, cellIndex);
-                roundResult = GameController.checkWin(cellIndex, players[currentPlayer].playerMarker, game.DOMBoardCache)
+                let roundResult = GameController.checkWin(cellIndex, players[currentPlayer].playerMarker, game.DOMBoardCache);
+
+                Object.values(game.DOMBoardCache).forEach(cell => {
+                   if (cell.innerHTML === '') 
+                        gameBoardFull = false;
+                });
+
+                if (gameBoardFull){
+                    winnerHeading.innerHTML = 'Draw!';
+                    statusModal.style.display = 'flex';
+                    statusModal.showModal();
+
+                    noTies++;
+                }
                 
-                if (roundResult){
-                    console.log('Win');
+                if (roundResult) {
+                    winnerHeading.innerHTML = `${players[currentPlayer].name} wins!`;
+                    players[currentPlayer].score++;
+                    statusModal.style.display = 'flex';
+                    statusModal.showModal();
                 }
 
                 if (isPlayer1){    
@@ -105,16 +180,6 @@ const GameController = (() => {
                 }
             })
         });
-
-        (function displayBoard(){
-            const gameContainer = document.querySelector('.game-container');
-
-            modal.style.display = 'none';
-            modal.close();
-            gameContainer.style.display = 'grid';
-        })();
-
-
 
     });
 
